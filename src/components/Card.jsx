4430d69ec309e81fe007f900de87cc205165d385
ahmadshehhab <ahmadshehab11177@outlook.sa>
2023-09-01 20:02:22 +0300

    asas

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+    <div className="CardContainer">
+        <div className="CardTitle">This is the post Title</div>
+        <div className="CardBody">This is the post body</div>
+        
+    </div>
+    
+    </>
+  )
+}
+
+export default Card
